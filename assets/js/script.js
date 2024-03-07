46591d191f96c367585e318b89171b929eab485a
finntendoverse <finnlambouris616@gmail.com>
2024-03-07 17:47:15 -0500

    added delete button to tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
-// Retrieve tasks and nextId from localStorage
 let taskList = [];
 let nextId = 0;
 
-// Todo: create a function to generate a unique task id
+// this function generates a unique task id that is saved in local storage
 function generateTaskId() {
     nextId++
     localStorage.setItem("nextId", nextId);
     return nextId;
 }
 
-// Todo: create a function to create a task card
+// this function creates the task card and appends it to the to-do section
 function createTaskCard(task) {
     let taskCard = document.createElement("p");
-    taskCard.setAttribute("class", "card-text");
+    taskCard.setAttribute("class", "task-card");
     taskCard.setAttribute("style", "background-color: green");
-    taskCard.innerHTML = task.task + "<br>" + task.description + "<br>" + task.deadline;
+    taskCard.innerHTML = task.task + "<br>" + task.description + "<br>" + task.deadline + "<br>" + `<button class="delete-button">delete</button`;
     document.querySelector("#todo-cards").appendChild(taskCard);
+    return taskCard;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+    $(".task-card").draggable();
 }
 
 // Todo: create a function to handle adding a new task
@@ -30,17 +30,22 @@ function handleAddTask(event) {
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-
+    let deleteButton = document.querySelectorAll(".delete-button");
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
-}
+    $("#in-progress-cards").droppable({
+        drop: function() {
+          task.setAttribute("status", "in progress");
+          console.log(task.status);
+        }
+    })
+};
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-    renderTaskList()
+    renderTaskList();
 
     let createTaskButton = document.querySelector("#create-task");
 
@@ -56,6 +61,7 @@ $(document).ready(function () {
         console.log(taskList);
         localStorage.setItem("tasks", JSON.stringify(taskList));
         createTaskCard(task);
+        renderTaskList();
     });
 
 
